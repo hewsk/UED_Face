ea1579356da11836f9d7c2734bdae009904cfb91
裴杨华 <xp@peiyanghuadeMacBook-Pro.local>
2020-02-29 19:09:37 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './home';
 import * as serviceWorker from './serviceWorker';
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 
